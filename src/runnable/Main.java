b6e1087c7ec90929ce7312e27dbbeb320dee0756
Akın ELDEN <akinelden@gmail.com>
2019-07-05 23:12:04 +0300

    Main and mission classes are completed.

--- src/runnable/Main.java
+++ src/runnable/Main.java
@@ -1,10 +1,6 @@
 package runnable;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.LinkedList;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -25,28 +21,31 @@ public class Main {
 		createCities(destsFileName, cities);
 		createWaggons(waggonsFileName, cities);
 		createLocomotives(locsFileName, cities);
-		createMissions(missionsFileName, missions);
+		createMissions(missionsFileName, missions, cities);
 		for(Mission mission : missions){
-			if(!mission.startMission()){
-				// TODO: write error message
-			}
-			if(!mission.goMidwayCity()){
-				// TODO: write error message
-			}
-			if(!mission.goTargetCity()){
-				// TODO: write error message
-			}
+			String result;
+			result = mission.startMission();
+			showErrorDialog(result);
+			result = mission.goMidwayCity();
+			showErrorDialog(result);
+			result = mission.goTargetCity();
+			showErrorDialog(result);
 		}
 		writeResult(outputFileName, cities);
+		showErrorDialog("Simulation is succesfully completed.\n Check the file named: '"+outputFileName+"' to see the results.");
 	}
 
+
 	private static void showErrorDialog(String errorText){
+		if(errorText == null){
+			return;
+		}
 		JFrame frame = new JFrame("TrainTransportation");
 		JOptionPane.showMessageDialog(frame, errorText, "Alert", JOptionPane.ERROR_MESSAGE);
 		System.exit(1);
 	}
 
-	// TODO: write create functions
+
 	private static void createCities(String fileName, LinkedList<City> cities){
 		BufferedReader reader = null;
 		try{
@@ -55,41 +54,117 @@ public class Main {
 			while(line!=null){
 				City c = new City(line);
 				cities.add(c);
+				line = reader.readLine();
 			}
+			reader.close();
 		}catch(FileNotFoundException e1){
-			
+			showErrorDialog("The file: " + fileName +" could not found.\nSystem exiting.");
 		}catch(IOException e2){
-
+			showErrorDialog("An error occurred while reading " + fileName +" file.\nSystem exiting.");
 		}
 	}
 
 	private static void createWaggons(String fileName, LinkedList<City> cities){
-
+		BufferedReader reader = null;
+		try{
+			reader = new BufferedReader(new FileReader(fileName));
+			String line = reader.readLine();
+			while(line!=null){
+				String[] splitted = line.split("\\s");
+				Waggon w = new Waggon(splitted[0]);
+				for(City c : cities){
+					if(c.getName().equals(splitted[1])){
+						c.pushWaggon(w);
+						break;
+					}
+				}
+				line = reader.readLine();
+			}
+			reader.close();
+		}catch(FileNotFoundException e1){
+			showErrorDialog("The file: " + fileName +" could not found.\nSystem exiting.");
+		}catch(IOException e2){
+			showErrorDialog("An error occurred while reading " + fileName +" file.\nSystem exiting.");
+		}catch(ArrayIndexOutOfBoundsException e3){
+			showErrorDialog("An error occurred while reading " + fileName +" file.\nOne more lines don't contain waggon name and/or city name.\nSystem exiting.");
+		}
 	}
 
 	private static void createLocomotives(String fileName, LinkedList<City> cities){
-
-	}
-
-	private static void createMissions(String fileName, LinkedList<Mission> missions){
-
-	}
-
-	private static void readAndCreateObjects(String destsFileName, String waggonsFileName, 
-	String locsFileName, String missionsFileName, LinkedList<City> cities, LinkedList<Mission> missions){
-		BufferedReader destReader, waggonsReader, locsReader, missionsReader;
+		BufferedReader reader = null;
 		try{
-			destReader = new BufferedReader(new FileReader(destsFileName));
-			waggonsReader = new BufferedReader(new FileReader(waggonsFileName));
-			locsReader = new BufferedReader(new FileReader(locsFileName));
-			missionsReader = new BufferedReader(new FileReader(missionsFileName));
-		}catch(FileNotFoundException e){
+			reader = new BufferedReader(new FileReader(fileName));
+			String line = reader.readLine();
+			while(line!=null){
+				String[] splitted = line.split("\\s");
+				Locomotive l = new Locomotive(splitted[0],Double.parseDouble(splitted[2]));
+				for(City c : cities){
+					if(c.getName().equals(splitted[1])){
+						c.offerLocomotive(l);
+						break;
+					}
+				}
+				line = reader.readLine();
+			}
+			reader.close();
+		}catch(FileNotFoundException e1){
+			showErrorDialog("The file: " + fileName +" could not found.\nSystem exiting.");
+		}catch(IOException e2){
+			showErrorDialog("An error occurred while reading " + fileName +" file.\nSystem exiting.");
+		}catch(ArrayIndexOutOfBoundsException e3){
+			showErrorDialog("An error occurred while reading " + fileName +" file.\nOne more lines don't contain locomotive name, city name and/or locomotive power.\nSystem exiting.");
+		}catch(NumberFormatException e4){
+			showErrorDialog("An error occurred while reading " + fileName +" file.\nLocomotive power values are not double compatible.\nSystem exiting.");
+		}
+	}
 
+	private static void createMissions(String fileName, LinkedList<Mission> missions, LinkedList<City> cities){
+		BufferedReader reader = null;
+		try{
+			reader = new BufferedReader(new FileReader(fileName));
+			String line = reader.readLine();
+			int i=1;
+			while(line!=null){
+				Mission m = new Mission(i);
+				if(!m.parseMissionString(line, cities)){
+					showErrorDialog("An error occurred while reading the file: "+fileName+".\nMissions are not properly defined.");
+				}
+				missions.add(m);
+				i++;
+				line = reader.readLine();
+			}
+			reader.close();
+		}catch(FileNotFoundException e1){
+			showErrorDialog("The file: " + fileName +" could not found.\nSystem exiting.");
+		}catch(IOException e2){
+			showErrorDialog("An error occurred while reading " + fileName +" file.\nSystem exiting.");
 		}
 	}
 
 	private static void writeResult(String outputFileName, LinkedList<City> cities){
-		BufferedWriter outputWriter;
+		BufferedWriter writer;
+		try{
+			writer = new BufferedWriter(new FileWriter(outputFileName));
+			for(City c : cities){
+				writer.write(c.getName()+"\n");
+				writer.write("Waggon Garage:\n");
+				Waggon w = c.popWaggon();
+				while(w!=null){
+					writer.write(w.getName()+"\n");
+					w = c.popWaggon();
+				}
+				writer.write("Locomotive Garage:\n");
+				Locomotive l = c.popLocomotive();
+				while(l!=null){
+					writer.write(l.getName()+"\n");
+					l = c.popLocomotive();
+				}
+				writer.write("-------------\n");
+			}
+			writer.close();
+		}catch (IOException e){
+			showErrorDialog("An error occurred while writing the output file.\nPlease try again.");
+		}
 
 	}
 
--- src/transportation/Mission.java
+++ src/transportation/Mission.java
@@ -4,37 +4,66 @@ import java.util.LinkedList;
 
 public class Mission {
 
+    int missionNo;
     City starting, midway, target;
     int fromStarting, fromMidway;
 	LinkedList<Integer> indexes = new LinkedList<>();
     Train train = new Train();
 
-	// TODO: write this function
-	public boolean parseMissionString(String mission, LinkedList<City> cities){
+    public Mission(int i){
+        missionNo = i;
+    }
+
+	public boolean parseMissionString(String line, LinkedList<City> cities){
+        String[] splitted = line.split("-");
+        if(splitted.length != 6){
+            return false;
+        }
+        try{
+            for(City c : cities) {
+                if (c.getName().equals(splitted[0])) {
+                    starting = c;
+                }
+                else if(c.getName().equals(splitted[1])){
+                    midway = c;
+                }
+                else if(c.getName().equals(splitted[2])){
+                    target = c;
+                }
+            }
+            fromStarting = Integer.parseInt(splitted[3]);
+            fromMidway = Integer.parseInt(splitted[4]);
+            String[] splittedIndexes = splitted[5].split(",");
+            for(String s : splittedIndexes){
+                indexes.add(Integer.parseInt(s));
+            }
+        }catch(NumberFormatException e){
+            return false;
+        }
 		return true;
     }
 
-    public boolean startMission(){
+    public String startMission(){
         Locomotive l = starting.popLocomotive();
         if(l == null){
-            return false;
+            return "Mission "+Integer.toString(missionNo)+"- Starting city:"+starting+" doesn't have any locomotive.\nCan't start mission.\nSystem exiting.";
         }
         train.setLocomotive(l);
         for(int i=0; i<fromStarting; i++){
             Waggon w = starting.popWaggon();
             if(w==null){
-                return false;
+                return "Mission "+Integer.toString(missionNo)+"- Starting city:"+starting+" doesn't have enough waggons.\nCan't start mission.\nSystem exiting.";
             }
             train.addWaggon(w);
         }
-        return true;
+        return null;
     }
 
-    public boolean goMidwayCity(){
+    public String goMidwayCity(){
         for(int i=0; i<fromMidway; i++){
             Waggon w = midway.popWaggon();
             if(w==null){
-                return false;
+                return "Mission "+Integer.toString(missionNo)+"- Midway city:"+midway+" doesn't have enough waggon.\nCan't continue mission.\nSystem exiting.";
             }
             train.addWaggon(w);
         }
@@ -42,14 +71,14 @@ public class Mission {
         for(Integer index : indexes){
             Waggon w = train.removeWaggon(index);
             if(w==null){
-                return false;
+                return "Mission "+Integer.toString(missionNo)+"- Train doesn't have any waggon at specified indexes.\nCan't continue mission.\nSystem exiting.";
             }
             midway.pushWaggon(w);
         }
-        return true;
+        return null;
     }
 
-    public boolean goTargetCity(){
+    public String goTargetCity(){
         target.offerLocomotive(train.removeLocomotive());
         while(true){
             Waggon w = train.removeWaggon(0);
@@ -58,7 +87,7 @@ public class Mission {
             }
             target.pushWaggon(w);
         }
-        return true;
+        return null;
     }
     
 }
